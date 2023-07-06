910749b6c5207b066657ca1a5c580895d4359c9e
Parag Nagwekar <pnagweka@amazon.com>
2023-07-06 15:54:14 -0700

    adding CIDR for Prod3 and Stage3 account

--- prod/terraform/main.tf
+++ prod/terraform/main.tf
@@ -1,6 +1,8 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
+resource "aws_config_config_rule" "r" {
+  name = "vpc_flow_log_guardrail"
+  source {
+    owner             = "AWS"
+    source_identifier = "VPC_FLOW_LOGS_ENABLED"
+  }
 }
+
