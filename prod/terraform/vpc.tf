0d9d84aeaaca8e145fa6b48b31dfe383c2760725
Parag Nagwekar <pnagweka@amazon.com>
2023-07-08 16:51:02 -0700

    Adding subnets

--- prod/terraform/vpc.tf
+++ prod/terraform/vpc.tf
@@ -9,3 +9,20 @@ resource "aws_vpc" "main" {
    Name = "sandbox VPC"
  }
 }
+
+resource "aws_subnet" "sub01" {
+  vpc_id     = "${aws_vpc.main.id}"
+  # 10.1.0.0/21
+  cidr_block = "${cidrsubnet(data.aws_ssm_parameter.cidr_block.value, 3, 1)}"
+  tags {
+    Name = "01"
+  }
+}
+resource "aws_subnet" "sub02" {
+  vpc_id     = "${aws_vpc.main.id}"
+  # 10.1.0.0/21
+  cidr_block = "${cidrsubnet(data.aws_ssm_parameter.cidr_block.value, 3, 2)}"
+  tags {
+    Name = "02"
+  }
+}
