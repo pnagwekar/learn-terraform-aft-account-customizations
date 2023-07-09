6f1c496e62cc336a6b1b23bb97da05fa26903ced
Parag Nagwekar <pnagweka@amazon.com>
2023-07-08 17:24:16 -0700

    Adding subnets for all VPCs

--- stage/terraform/vpc.tf
+++ stage/terraform/vpc.tf
@@ -6,6 +6,30 @@ resource "aws_vpc" "main" {
  cidr_block = data.aws_ssm_parameter.cidr_block.value
  
  tags = {
-   Name = "sandbox VPC"
+   Name = "Staging VPC"
  }
+}
+
+resource "aws_subnet" "sub01" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${cidrsubnet(data.aws_ssm_parameter.cidr_block.value, 3, 1)}"
+  tags = {
+    Name = "Private-Subnet-01"
+  }
+}
+
+resource "aws_subnet" "sub02" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${cidrsubnet(data.aws_ssm_parameter.cidr_block.value, 3, 2)}"
+  tags = {
+    Name = "Private-Subnet-02"
+  }
+}
+
+resource "aws_subnet" "sub03" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${cidrsubnet(data.aws_ssm_parameter.cidr_block.value, 3, 3)}"
+  tags = {
+    Name = "Private-Subnet-03"
+  }
 }
